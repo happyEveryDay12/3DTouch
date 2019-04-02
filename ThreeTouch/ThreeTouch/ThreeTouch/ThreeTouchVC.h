41f9b7fb8117f050291451d3a7e47ba74d4149ca
樊建政 <fanjianzheng@synjones.net>
2019-04-02 17:47:08 +0800

    代码提交

---
+++ ThreeTouch/ThreeTouch/ThreeTouch/ThreeTouchVC.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+
+@interface ThreeTouchVC : UIViewController
+
+@property (nonatomic, strong)NSMutableArray *arrData;
+
+@property (nonatomic, assign)NSInteger index;
+
+@property (nonatomic, copy)NSString *strInfo;
+
+@end
